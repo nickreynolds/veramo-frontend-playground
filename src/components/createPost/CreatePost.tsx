714e886e16f5669ef816ac17b365070fbb126919
nickreynolds <nicholas.s.reynolds@gmail.com>
2022-04-20 14:54:23 -0400

    feat: Add rendering of HtmlPost credential

--- src/components/createPost/CreatePost.tsx
+++ src/components/createPost/CreatePost.tsx
@@ -19,6 +19,7 @@ import { constructHtmlPost } from './vcHelper'
 import { EthereumAccountContext } from '../../App'
 import { getAgent } from '../../veramo/setup'
 import { ethers } from "ethers";
+import { Element, Leaf } from './renderHelper'
 
  
 export type BlockQuoteElement = {
@@ -192,7 +193,7 @@ const RichTextExample = () => {
         const signer = await provider.getSigner();
         const agent = getAgent(signer);
         const cred = await agent.createVerifiableCredentialEIP712({ credential, ethereumAccountId: ethereumAddress });
-        console.log("cred: ", cred);
+        console.log("cred: ", JSON.stringify(cred));
 
         await agent.dataStoreSaveMessage({ message: cred })
       }}>Issue post</button>
@@ -264,74 +265,6 @@ const isBlockActive = (editor, format, blockType = 'type') => {
 const isMarkActive = (editor, format) => {
   const marks = Editor.marks(editor)
   return marks ? marks[format] === true : false
-}
-
-const Element = ({ attributes, children, element }) => {
-  const style = { textAlign: element.align }
-  switch (element.type) {
-    case 'block-quote':
-      return (
-        <blockquote style={style} {...attributes}>
-          {children}
-        </blockquote>
-      )
-    case 'bulleted-list':
-      return (
-        <ul style={style} {...attributes}>
-          {children}
-        </ul>
-      )
-    case 'heading-one':
-      return (
-        <h1 style={style} {...attributes}>
-          {children}
-        </h1>
-      )
-    case 'heading-two':
-      return (
-        <h2 style={style} {...attributes}>
-          {children}
-        </h2>
-      )
-    case 'list-item':
-      return (
-        <li style={style} {...attributes}>
-          {children}
-        </li>
-      )
-    case 'numbered-list':
-      return (
-        <ol style={style} {...attributes}>
-          {children}
-        </ol>
-      )
-    default:
-      return (
-        <p style={style} {...attributes}>
-          {children}
-        </p>
-      )
-  }
-}
-
-const Leaf = ({ attributes, children, leaf }) => {
-  if (leaf.bold) {
-    children = <strong>{children}</strong>
-  }
-
-  if (leaf.code) {
-    children = <code>{children}</code>
-  }
-
-  if (leaf.italic) {
-    children = <em>{children}</em>
-  }
-
-  if (leaf.underline) {
-    children = <u>{children}</u>
-  }
-
-  return <span {...attributes}>{children}</span>
 }
 
 const BlockButton = ({ format, icon }) => {
